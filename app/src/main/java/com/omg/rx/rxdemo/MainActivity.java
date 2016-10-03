adec6e0df5b3c399d9b4b34620bee2b89a6fbb31
Mahesh Gujare <mgujare@yahoo-inc.com>
2016-10-02 22:14:04 -0700

    adding travis file for CI

--- app/src/main/java/com/omg/rx/rxdemo/MainActivity.java
+++ app/src/main/java/com/omg/rx/rxdemo/MainActivity.java
@@ -7,13 +7,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.jakewharton.rxbinding.view.RxView;
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.SearchResponse;
+import com.yelp.clientlib.entities.options.CoordinateOptions;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Call;
 import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -28,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private CompositeSubscription _compositeSubscription = new CompositeSubscription();
     private TextView textView;
+    private String consumerKey = "FsxTpwjRk15suwXZfeG7Og";;
+    private String consumerSecret = "DZ6JHCcW3GldLFzYWKfV5O44GcI";
+    private String token = "O9J2OU-3_fsTN6LHLZdAJKzztwhzQT7B";
+    private String tokenSecret = "y6lYOZu6udKDJn3r5mMgZIeDD0U";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +74,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public Observable<JSONObject> call() {
                 try {
-                    return Observable.just(getDealData());
-                } catch (InterruptedException | ExecutionException |
-                IOException | JSONException e){
-                    Log.e("deals", e.getMessage());
+                    return Observable.just(getYelpData());
+                } catch (JSONException e) { //InterruptedException | ExecutionException | IOException |
+                            Log.e("deals", e.getMessage());
                     return Observable.error(e);
                 }
             }
@@ -85,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onError(Throwable e) {
                     Log.e(TAG, "onError");
-                    textView.setText("Error");
+                    textView.setText("Error " + e.toString());
                 }
 
                 @Override
@@ -107,5 +118,27 @@ public class MainActivity extends AppCompatActivity {
         Response response = client.newCall(request).execute();
         JSONObject jsonObject = new JSONObject(response.body().string());
         return jsonObject;
+    }
+
+    private JSONObject getYelpData() throws JSONException {
+        String result = null;
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("term", "restaurant");
+        queryParams.put("limit", "3");
+        CoordinateOptions coordinate = CoordinateOptions.builder()
+                .latitude(37.7749)
+                .longitude(-122.4194).build();
+        YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey, consumerSecret, token, tokenSecret);
+        YelpAPI yelpAPI = apiFactory.createAPI();
+        Call<SearchResponse> call = yelpAPI.search(coordinate, queryParams);
+        try {
+            retrofit2.Response<SearchResponse> response = call.execute();
+            Log.d(TAG + "error", " " + response.isSuccessful());
+            Log.d(TAG + "res", response.raw().toString());
+            result = response.raw().toString();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return new JSONObject(result);
     }
 }
